57df5b9776e28dbf26af7818aeb2cb7ce1602970
Császár Zsolt <csaszarzsolti.92@gmail.com>
2022-06-24 07:19:30 +0200

    Main

---
+++ InvoiceEasy/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using InvoiceEasy.Models;
+
+namespace InvoiceEasy
+{
+    public class DataContext    : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):
+            base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseSerialColumns();
+        }
+
+        public DbSet<InvoiceModel> Invoices { get; set; }
+        public DbSet<InvoiceItemModel> InvoiceItems { get; set; }
+        public DbSet<PersonModel> Persons { get; set; }
+        public DbSet<CompanyModel> Companies { get; set; }
+    }
+}
---
+++ InvoiceEasy/TestData.cs
@@ -0,0 +1,38 @@
+﻿using InvoiceEasy.Models;
+
+namespace InvoiceEasy
+{
+    public static class TestData
+    {
+        public static void Seed(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            using var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+            context.Database.EnsureCreated();
+            AddData(context);
+        }
+
+        private static void AddData(DataContext context)
+        {
+            var data = context.Invoices.FirstOrDefault();
+            if (data != null) return;
+
+            context.Invoices.Add(new InvoiceModel
+            {
+                uniqueNumber = 157289,
+                seller = new PersonModel { firstName = "Patrik", lastName = "Pelda", address = "test"},
+                buyer = new PersonModel { firstName = "Peter", lastName = "Pelda", address = "test2" },
+                invoiceItems = new List<InvoiceItemModel> {
+                    new InvoiceItemModel {
+                        name = "Krumpli",
+                        cost = 400,
+                        quantity = 10,
+                        description = "Pityoka",
+                        isActive = true
+                    } }
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
